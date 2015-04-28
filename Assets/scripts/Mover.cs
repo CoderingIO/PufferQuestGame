3171b963ae7e77a4a1852cb4c31002beae913ff5
Eddie Ball <atomicdogindustries@gmail.com>
2015-04-27 19:05:17 -0600

    4/27/2015

---
+++ Assets/scripts/Mover.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Rigidbody2D> ().velocity = transform.up * speed;
+	}
+}
