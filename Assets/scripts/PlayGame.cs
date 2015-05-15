de546dc58cef1a40b3bfb48fb184b1fd49ba255d
Eddie Ball <atomicdogindustries@gmail.com>
2015-05-15 09:51:23 -0600

    commit eballz

--- Assets/scripts/PlayGame.cs
+++ Assets/scripts/PlayGame.cs
@@ -1,11 +1,31 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//using UnityEngine;
+//using System.Collections;
 
-public class PlayGame : MonoBehaviour {
+//public class PlayGame : MonoBehaviour {
 
 	
 	// Update is called once per frame
-	public void OnClickEnter () {
-		Application.LoadLevel ("pufferConcept");
+	//public void OnClickEnter () {
+	//	Application.LoadLevel ("Stage Select");
+	//}
+//}
+
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour {
+	
+	public InputField playerName;
+	
+	public void startGame ()
+	{
+		
+		PlayerPrefs.SetString ("playerName", playerName.text);
+		Application.LoadLevel("WaynesAIzone");
+		Debug.Log ("level is noticed");
 	}
+	
+	
 }
